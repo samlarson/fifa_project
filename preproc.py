9249449ed9fa70c7ab34bad17757031a1afa4f43
Unknown <slarson207@gmail.com>
2019-04-22 15:04:05 -0400

    Added score_regex v1 function

--- preproc.py
+++ preproc.py
@@ -41,6 +41,48 @@ def preproc_euros(df):
     return df
 
 
+def score_regex(df):
+    for col in df.iloc[:, 28:54]:
+        for cell in df[col]:
+            if cell == 0 or cell == '':
+                continue
+            elif '+' not in cell:
+                continue
+            else:
+                score = str(cell)
+                score_one, score_two = score.split('+')
+                # print(score_one, ' ', score_two)
+                df.loc[cell, col] = float(score_one) + float(score_two)
+                print(df.loc[cell, col])
+                # print(type(score))
+                # df.loc[cell, col] = sum(map(int, df.split("+")))
+
+    return df
+
+
+def test_fxn(x):
+    if x == 0 or x == '':
+        return x
+    elif '+' not in x:
+        return x
+    else:
+        score_one, score_two = x.split('+')
+        score = int(score_one) + int(score_two)
+        # print(type(score))
+        return score
+        # return sum(map(int, x.split("+")))
+
+
+def test(df):
+    for col in df.iloc[:, 28:54]:
+        for cell in col:
+            # df[col].map(test_fxn)
+            df.loc[cell, col] = test_fxn(cell)
+
+
+    return df
+
+
 # Fully print a Pandas dataframe
 def full_print(df):
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
@@ -48,13 +90,16 @@ def full_print(df):
 
 
 data_path = 'data/ex_fifa.csv'
-# fifa_df = preproc_euros(open_file(load_data(data_path=data_path)))
 fifa_df = load_data(data_path=data_path)
-keep = ['Name', 'Value', 'Wage', 'Release Clause']
-fifa_df = fifa_df[keep]
 
-full_print(fifa_df)
+# keep = ['Name', 'Value', 'Wage', 'Release Clause']
+# fifa_df = fifa_df[keep]
 
-# TODO: convert categorical variables/one-hot encode, join string addition variables
-# drop_cols = ['Photo', 'Flag', 'Club Logo', 'Real Face', ]
-# df.drop(columns=drop_cols)
+score_regex(fifa_df)
+
+full_print(fifa_df['LS'])
+
+# TODO: multiply currency values, convert categorical variables/one-hot encode, join string addition variables
+# lame_cols = ['Photo', 'Flag', 'Club Logo', 'Real Face', ]
+# df.drop(columns=lame_cols)
+
